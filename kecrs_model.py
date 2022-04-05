7a9751eb799a103634e81ffa4ff319354f215335
huyquangdao <=>
2022-04-05 21:58:26 +0700

    fix bug

--- kecrs_model.py
+++ kecrs_model.py
@@ -288,7 +288,7 @@ class CrossModel(nn.Module):
             opt["embedding_size"] + opt["embedding_size"], opt["embedding_size"]
         )
 
-        self.entity_selection_bias = nn.Linear(
+        self.entity_bias = nn.Linear(
             opt["dim"], opt["n_entity"]
         )
 
@@ -506,7 +506,7 @@ class CrossModel(nn.Module):
             )
         )
         # #B, S, entity_dim
-        entity_score_1 = torch.softmax(F.linear(entity_score, db_nodes_features), dim =1)
+        entity_score_1 = torch.softmax(F.linear(entity_score, db_nodes_features, entity_bias.bias), dim =1)
         # #B, S, n_entities
         entity_score_2 = torch.sigmoid(torch.sum(entity_score_1, dim = 1))
         # #B, n_entities
